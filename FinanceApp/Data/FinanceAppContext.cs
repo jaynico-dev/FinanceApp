6799fae76f016dc6691ef94b615c8890fdc1e6ec
JNico <nicoolano43@gmail.com>
2025-08-09 22:09:39 +0800

    Add project files.

---
+++ FinanceApp/Data/FinanceAppContext.cs
@@ -0,0 +1,16 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    // This class represents the bridge between the application and the database.
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options)
+        {
+
+        }
+
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Amount needs to be heigher than 0")]
+        public string Description { get; set; } = null!;
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
